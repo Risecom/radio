f87f7a27b24d52f6c488ff0ea0a4e6ca63ef3767
Risecom <rise01@me.com>
2013-07-29 22:52:28 +0400

    5

---
+++ 5/index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<title>Радио</title> <!-- Page title -->
+	<link rel="stylesheet" href="style.css" type="text/css" /> <!-- CSS -->
+	<link rel="apple-touch-icon-precomposed" href="icon-iphone.png" /> <!-- iPhone icon -->
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="icon-iphone-retina.png" /> <!-- iPhone icon-retina -->
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="icon-pad.png" /> <!-- iPad icon -->
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="icon-ipad-retina.png" /> <!-- iPad icon-retina -->
+	<meta name="viewport" content="user-scalable=no, width=device-width" /> <!-- Zoom off -->
+	<script>function NoElasticScroll(e) {e.preventDefault();}</script> <!-- Scroll off -->
+	<meta http-equiv="Content-Type" content="text/html; charset=windows-1251"> <!-- Cyrillic -->
+</head>
+<body ontouchmove="NoElasticScroll(event);">
+<!-- Author: Nikita Zybarev -->
+<table border="0" class="table">
+	<tr>
+		<td align="center"><div class="text">iTunes Radio</div></td>
+	</tr>
+	<tr>
+		<td align="center"><a href="https://itunes.apple.com/us/tv-season/boss-season-1/id520867941"><img src="on.png" class="item" /></a></td>
+	</tr>
+	<tr>
+		<td align="center"><a href="https://itunes.apple.com/ru/movie/utomlennye-solncem/id580293033"><img src="off.png" class="item1" /></a></td>
+	</tr>
+</table>
+</body>
+</html>
